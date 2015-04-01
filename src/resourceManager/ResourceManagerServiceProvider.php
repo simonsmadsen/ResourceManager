25134d76c3ff71866c71086af3f8647e0d63965b
Simon Madsen <simon@smadsen.com>
2015-04-01 04:10:23 +0200

    routes moved

--- src/resourceManager/ResourceManagerServiceProvider.php
+++ src/resourceManager/ResourceManagerServiceProvider.php
@@ -22,8 +22,46 @@ class ResourceManagerServiceProvider extends ServiceProvider {
 
         $this->loadViewsFrom(__DIR__.'/views', 'resourceManager');
 
-        include __DIR__.'/ResourceRoutes.php';
+        $this->routes();
+    }
 
+    private function routes(){
+        Route::get('/res/{resource?}', function($resource = null){
+
+            $viewBag = config('resourceManager.viewbag');
+            $viewBag['r'] = new ResourceViewer($resource);
+
+
+            return view('resourceManager::welcome',$viewBag);
+
+        });
+
+        Route::post('/res/update',function(){
+
+            $modelName = Input::get('resource_model');
+            $model = new $modelName;
+            $model = $model->find(Input::get('id'));
+            $model->fill(Input::all());
+            $model->update();
+            return Redirect::back();
+        });
+
+        Route::post('/res/create',function(){
+
+
+            $modelName = Input::get('resource_model');
+            $model = new $modelName(Input::all());
+            $model->save();
+
+            return Redirect::back();
+        });
+
+        Route::get('/res/delete/{id}/{model}',function($id,$model){
+            $model = str_replace('_','\\',$model);
+            $model = new $model;
+            $model->destroy($id);
+            return Redirect::back();
+        });
     }
 
     private function publishConfig(){
